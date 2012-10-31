74e9fe1ddfe771e8bfd60bf4c69e930e1d1f020d
Frank van den Brink <frank@frankvandenbrink.nl>
2012-10-31 15:01:44 +0100

    Fixed compatibility errors.

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
     # Install PHP packages
-    $phpPackages = ["php5", "php5-cli", "php5-mysql", "php-pear", "php5-dev", "php-apc", "php5-mcrypt", "php5-gd", "php5-sqlite", "php5-curl", "php5-intl", "php5-xdebug", "php5-memcache", "php5-imagick"]
+    $phpPackages = ["php5", "php5-cli", "php5-mysql", "php-pear", "php5-dev", "php-apc", "php5-mcrypt", "php5-gd", "php5-sqlite", "php5-curl", "php5-intl", "php5-xdebug", "php5-memcache", "php5-imagick", "libapache2-mod-php5"]
     package { $phpPackages:
         ensure => latest,
         require => [Exec['apt-get update'], Package['apache2']],
@@ -14,13 +14,13 @@ class php {
         source => "puppet:///modules/php/php-cli.ini",
         require => Package['php5-cli'],
     }
-    file { "/etc/php5/apache2filter/php.ini":
+    file { "/etc/php5/apache2/php.ini":
         ensure => file,
         owner => "root",
         group => "root",
         source => "puppet:///modules/php/php-web.ini",
         notify => Service["apache2"],
-        require => Package['php5'],
+        require => Package['libapache2-mod-php5'],
     }
 
     # Ensure session folder is writable by Vagrant user (under which apache runs)
