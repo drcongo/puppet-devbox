f85430c3c9f63697f8615d689f8ce5a8d5991906
Marcus Sa <marcusesa@gmail.com>
2013-01-24 00:54:41 -0200

    Replace documentroot in apache conf and enable vhosts.

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,4 +1,4 @@
-class apache ($hostname) {
+class apache ($hostname, $documentroot) {
     # Install apache
     package { "apache2":
         ensure => latest,
@@ -20,7 +20,7 @@ class apache ($hostname) {
         require => Package["apache2"],
         subscribe => [
             File["/etc/apache2/mods-enabled/rewrite.load"],
-            File["/etc/apache2/sites-enabled/010-project"]
+            File["/etc/apache2/sites-available/010-project"]
         ]
     }
 
@@ -32,24 +32,36 @@ class apache ($hostname) {
     }
 
     # Set the configuration
-    file { "/etc/apache2/sites-enabled/010-project":
+    file { "/etc/apache2/sites-available/010-project":
         ensure => present,
         source => "puppet:///modules/apache/project_vhost.conf",
         replace => false,
         require => Package['apache2'],
     }
 
-    # Set the hostname
+    # Set the hostname and documentroot
     exec { "apache.hostname":
         command => "echo \"ServerName localhost\" >> /etc/apache2/httpd.conf",
         unless => "grep ServerName /etc/apache2/httpd.conf",
         require => Package['apache2'],
         notify  => Service["apache2"]
     }
+
     exec { "apache.project.hostname":
-        command => "sed -i 's/ServerName __HOSTNAME__/ServerName $hostname/' /etc/apache2/sites-enabled/010-project",
-        onlyif => "grep \"ServerName __HOSTNAME__\" /etc/apache2/sites-enabled/010-project",
-        require => File['/etc/apache2/sites-enabled/010-project']
+        command => "sed -i 's/ServerName __HOSTNAME__/ServerName $hostname/' /etc/apache2/sites-available/010-project",
+        onlyif => "grep \"ServerName __HOSTNAME__\" /etc/apache2/sites-available/010-project",
+        require => File['/etc/apache2/sites-available/010-project']
+    }
+
+    exec { "apache.project.documentroot":
+	command => "sed -i 's/__DOCUMENTROOT__/$documentroot/g' /etc/apache2/sites-available/010-project",
+        onlyif => "grep \"DocumentRoot /vagrant/__DOCUMENTROOT__\" /etc/apache2/sites-available/010-project",
+        require => File['/etc/apache2/sites-available/010-project']
+    }
+
+    exec { "apache.enable.virtualhosts":
+        command => "/usr/sbin/a2dissite 000-default && /usr/sbin/a2ensite 010-project && /etc/init.d/apache2 reload",
+        require => File['/etc/apache2/sites-available/010-project'],
     }
 
     # Remove the default index file
