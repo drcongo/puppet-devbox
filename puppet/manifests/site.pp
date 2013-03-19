88fdf9a486c4d8b8fd8872862ba5841de75c3fc3
Andy Beaumont <andy@andybeaumont.com>
2013-03-19 11:04:41 +0000

    Updated site manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,6 +1,6 @@
 class { "devbox":
     hostname => "testbox2.dev", # Make sure this maps to the address above
     documentroot => "site/public", # Apache documentroot eg: www, web, public_html etc
-    gitUser => "drcongo",
-    gitEmail => "andy@andybeaumont.com"
+    gitUser => "Your github username",
+    gitEmail => "Your github email address"
 }
